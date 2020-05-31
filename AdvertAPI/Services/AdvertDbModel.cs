633f6feb3f388002a11543c376cc7f3db79fba88
Brian Blank <bblank@vvisions.net>
2020-05-31 17:50:53 -0400

    Initial Commit

---
+++ AdvertAPI/Services/AdvertDbModel.cs
@@ -0,0 +1,23 @@
+﻿using AdvertAPI.Models;
+using System;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace AdvertAPI.Services
+{
+    [DynamoDBTable("Advert")]
+    public class AdvertDbModel
+    {
+        [DynamoDBHashKey]
+        public String Id { get; set; }
+        [DynamoDBProperty]
+        public string Title { get; set; }
+        [DynamoDBProperty]
+        public string Description { get; set; }
+        [DynamoDBProperty]
+        public double Price { get; set; }
+        [DynamoDBProperty]
+        public DateTime CreationDateTime { get; set; }
+        [DynamoDBProperty]
+        public AdvertStatus Status { get; set; }
+    }
+}
